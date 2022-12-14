4294655193c1f3dc7c147e1bfab9b3259ab48d30
Dimitar Byalkov <dbyalkov94@gmail.com>
2022-12-14 11:37:24 +0200

    AddedStatisticsServiceUnitTests

---
+++ WebApp501.Tests/UnitTests/StatisticsServiceUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using WebApp501.Core.Contracts;
+using WebApp501.Core.Services;
+using WebApp501.Infrastructure.Data.Entities;
+
+namespace WebApp501.Tests.UnitTests
+{
+    [TestFixture]
+    public class StatisticsServiceUnitTests : UnitTestsBase
+    {
+        private IStatisticsService statisticsService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            this.statisticsService = new StatisticsService(this.repo);
+        }
+
+        [Test]
+        public async Task Total_ShouldReturnCorrectCounts()
+        {
+            //Arrange
+
+            //Act
+            var statistics = await this.statisticsService.Total();
+
+            //Assert
+            Assert.IsNotNull(statistics);
+
+            //Assert
+            var totalCocktails = this.repo.AllReadonly<Cocktail>()
+                .Where(c => !c.IsDeleted)
+                .Count();
+
+            var totalBartenders = this.repo.AllReadonly<Bartender>()
+                .Count();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(totalCocktails, Is.EqualTo(statistics.TotalCocktails));
+                Assert.That(totalBartenders, Is.EqualTo(statistics.TotalBartenders));
+            });
+        }
+    }
+}
